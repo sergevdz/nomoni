9933504aa2386c9c67e4714f2dc3853065127d6b
Sergio Valdez <sergioavr93@gmail.com>
2021-02-20 18:42:27 -0600

    Componente para crear paneles

---
+++ web/src/boot/contentPanel.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import ContentPanel from '../components/ContentPanel.vue'
+
+Vue.component('content-panel', ContentPanel)
---
+++ web/src/components/ContentPanel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="q-pa-md bg-grey-3">
+    <div :class="`row bg-${color} border-panel`">
+      <div class="col q-pa-md">
+         <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentPanel',
+  props: {
+    color: {
+      type: String,
+      default: 'white'
+    }
+  }
+}
+</script>
